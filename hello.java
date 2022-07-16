0ba55369f7b71908215316292226d26d20cb9204
Ragul <ragulvs01@gmail.com>
2022-07-16 11:35:11 +0530

    First task

---
+++ hello.java
@@ -0,0 +1,9 @@
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println("hii");
+		System.out.println("how are you?");
+		System.out.println("What are you doing now");
+	}
+}
---
+++ typecasting.java
@@ -0,0 +1,10 @@
+
+public class typecasting {
+
+	public static void main(String[] args) {
+		int v=99;
+		char s=(char)v;
+		System.out.println("The output is" +s);
+	}
+
+}
